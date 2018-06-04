a4aeabc306b4c298362cec32d9a4e1047f8447cf
hrhr0909 <toremoro0909@ezweb.ne.jp>
2018-06-04 19:16:19 +0900

    最初のコミット

---
+++ 0604/Assets/ArrowGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour {
+
+    public GameObject arrowPrefab;
+    float span = 1.0f;
+    float delte = 0;
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        this.delte += Time.deltaTime;
+        if(this.delte > this.span)
+        {
+            this.delte = 0;
+            GameObject go = Instantiate(arrowPrefab) as GameObject;
+            int px = Random.Range(-6, 7);
+            go.transform.position = new Vector3(px, 7, 0);
+        }
+
+		
+	}
+}
